1a3a0f360753a88f42c9e5e403a9807a662957fc
stephens-dev <james.stephens@bryanuniversity.edu>
2021-10-06 18:25:39 -0500

    Update quick_sort.py

--- week6/quick_sort.py
+++ week6/quick_sort.py
@@ -84,7 +84,9 @@ def partition(a_list, start, end):
     # Start at the first element past the pivot point
     low = start + 1
     high = end
+    
     while True:
+        
         # If the current value we're looking at is larger than the pivot
         # it's in the right place (right side of pivot) and we can move left,
         # to the next element.
@@ -135,9 +137,7 @@ def partition_random(array, start, end):
         # or low is higher than high, in which case we exit the loop
         if low <= high:
             array[low], array[high] = array[high], array[low]
-            # The loop continues
         else:
-            # We exit out of the loop
             break
 
     array[start], array[high] = array[high], array[start]
@@ -152,196 +152,43 @@ def quicksort_random(array, start, end):
     quicksort_random(array, start, p-1)
     quicksort_random(array, p+1, end)
 
-print("Quick Sort:")
-myList = [54,26,93,17,77,31]
-myList1 = [54,26,93,17,77,31]
-myList2 = [54,26,93,17,77,31]
-myList3 = [54,26,93,17,77,31]
 
+myList = [x for x in range(1000)]
+random.shuffle(myList)
 
-# myList = [x for x in range(1000)]
-# random.shuffle(myList)
+myList1 = [x for x in range(1000)]
+random.shuffle(myList1)
 
-#print(myList)
+myList2 = [x for x in range(1000)]
+random.shuffle(myList2)
+
+myList3 = [x for x in range(1000)]
+random.shuffle(myList3)
+
+print()
 start_time = time.time()
 quick_sort(myList,0,len(myList)-1)
 end_time = time.time()
-#print()
-#print("Sorted Listed: ")
-print(myList)   
-print(f"The execution time is: {end_time-start_time}")
+# print(myList)   
+print(f"Start pivot time is: {end_time-start_time}")
+print()
 
 start_time = time.time()
 quicksort_last(myList1,0,len(myList1)-1)
 end_time = time.time()
-print(myList1)   
-print(f"The execution time is: {end_time-start_time}")
+# print(myList1)   
+print(f"Last pivot time is: {end_time-start_time}")
+print()
 
 start_time = time.time()
 quicksort_middle(myList2,0,len(myList2)-1)
 end_time = time.time()
-print(myList2)   
-print(f"The execution time is: {end_time-start_time}")
+# print(myList2)   
+print(f"Middle pivot time is: {end_time-start_time}")
+print()
 
 start_time = time.time()
 quicksort_random(myList3,0,len(myList3)-1)
 end_time = time.time()
-print(myList3)   
-print(f"The execution time is: {end_time-start_time}")
-
-# start_time = time.time()
-# quicksort_median(myList2,0,len(myList2)-1)
-# end_time = time.time()
-# print(myList2)   
-# print(f"The execution time is: {end_time-start_time}")
-
-# firstcomparison = 0
-# lastcomparison = 0
-# mediancomparison = 0
-
-#A method for partition around the first element of the array
-# def partition_first(array, leftend, rightend):
-    # pivot = array[leftend]
-    # i = leftend + 1
-    # for j in range(leftend + 1, rightend):
-    #     if array[j] < pivot:
-    #         temp = array[j]
-    #         array[j] = array[i]
-    #         array[i] = temp
-    #         i += 1
-
-    # leftendval = array[leftend]
-    # array[leftend] = array[i-1]
-    # array[i-1] = leftendval
-    # return i - 1 
-
-#A method for partitioning around the last element of the array
-# def partition_last(array, leftend, rightend):
-    
-#     pivot = array[rightend-1]
-
-#     array[rightend-1] = array[leftend]
-#     array[leftend] = pivot
-    
-#     i = leftend + 1
-#     for j in range(leftend + 1, rightend):
-#         if array[j] < pivot:
-#             temp = array[j]
-#             array[j] = array[i]
-#             array[i] = temp
-#             i += 1
-
-#     leftendval = array[leftend]
-#     array[leftend] = array[i-1]
-#     array[i-1] = leftendval
-#     return i - 1 
-
-# #A method to calculate the median of three numbers using two comparisons
-# def median(a, b, c):
-#     if ( a - b) * (c - a) >= 0:
-#         return a
-
-#     elif (b - a) * (c - b) >= 0:
-#         return b
-
-#     else:
-#         return c
-
-# #A method to partition around the median
-# def partition_median(array, leftend, rightend):
-#     left = array[leftend]
-#     right = array[rightend-1]
-#     length = rightend - leftend
-#     if length % 2 == 0:
-#         middle = array[leftend + length//2 - 1]
-#     else:
-#         middle = array[leftend + length//2]
-  
-    
-
-#     pivot = median(left, right, middle)
-
-#     pivotindex = array.index(pivot) #only works if all values in array unique
-
-#     array[pivotindex] = array[leftend]
-#     array[leftend] = pivot
-
-#     i = leftend + 1
-#     for j in range(leftend + 1, rightend):
-#         if array[j] < pivot:
-#             temp = array[j]
-#             array[j] = array[i]
-#             array[i] = temp
-#             i += 1
-
-#     leftendval = array[leftend]
-#     array[leftend] = array[i-1]
-#     array[i-1] = leftendval
-#     return i - 1 
-
-    
-# #Implement quicksort while partitioning around the first index
-# def quick_sort1(array, leftindex, rightindex):
-#     global firstcomparison
-#     if leftindex < rightindex:
-        
-#         newpivotindex = partition_first(array, leftindex, rightindex)
-        
-#         firstcomparison += (rightindex - leftindex - 1)
-        
-#         quick_sort1(array, leftindex, newpivotindex) 
-        
-#         quick_sort1(array, newpivotindex + 1, rightindex)
-        
-# def quicksort_last(array, leftindex, rightindex):
-#     global lastcomparison
-#     if leftindex < rightindex:
-
-#         newpivotindex = partition_last(array, leftindex, rightindex)
-
-#         lastcomparison += (rightindex - leftindex - 1)
-
-#         quicksort_last(array, leftindex, newpivotindex)
-#         quicksort_last(array, newpivotindex + 1, rightindex)
-
- 
-# def quicksort_median(array, leftindex, rightindex):
-#      global mediancomparison
-#      if leftindex < rightindex:
-
-#          newpivotindex = partition_median(array, leftindex, rightindex)
-
-#          mediancomparison += (rightindex - leftindex - 1)
-#          quicksort_median(array, leftindex, newpivotindex)
-         
-
-#          quicksort_median(array, newpivotindex + 1, rightindex)
-
-
-# test3 = [1, 11, 5, 15, 2, 12, 9, 99, 77, 0]
-# test4 = []
-# for i in range(1, 101):
-#     test4.append(i)
-
-# f = open("IntegerArray.txt", "r")
-# intarray = []
-# for line in f:
-#     intarray.append(int(line))
-# f.close()
-
-# myList = [x for x in range(1000)]
-# random.shuffle(myList)
-# start_time = time.time()
-# quick_sort(myList,0,len(myList)-1)
-# end_time = time.time()
-# #print()
-# #print("Sorted Listed: ")
-# #print(myList)   
-# print(f"The execution time is: {end_time-start_time}")
-
-#test on an array of length 10000
-# mediancomparison = 0
-# quicksort_median(test3, 0, len(test3))
-# print(test3)
-# print(mediancomparison)
-# quick_sort(myList,0,len(myList)-1)
+# print(myList3)   
+print(f"Random pivot time is: {end_time-start_time}")
